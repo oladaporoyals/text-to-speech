cf98c99c9336419b98113d04e77233f350d1be38
oladaporoyals <74567285+oladaporoyals@users.noreply.github.com>
2023-08-05 15:21:56 +0100

    Update basic-web-crawler.py

--- basic-web-crawler.py
+++ basic-web-crawler.py
@@ -1,1 +1,28 @@
+import re
+import requests
+def get_page_content(url):
+    r = requests.get(url)
+    html_text = r.text
+    html_text = re.sub("\s+"," ", html_text)
+    return html_text
 
+def extract_product _links(content:)
+    product_url_regex = re.complier(r'<article class="project_pod">.*?<h3>.*?<a href=)"(.*?)"')
+    result = re.findall(product_url_regex, content)
+links = ["http://books.toscrape.com/" + x for x in result]
+    return links
+
+
+def get_product_links():
+    url  "http://books.toscrape.com/catalogue/page-{}.html".format(page)
+    print(url)
+
+
+if __name__ == "__main__":
+    #url = "http://books.toscrape.com/index.html")
+    #content = get_page_content(url)
+    #links = extract_product_links(content)
+    #print(links)
+
+
+    get_all_product_links()
