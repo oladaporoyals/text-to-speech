4d3f4c0868d80e6d19bbfe0f6cc6518262c94426
oladaporoyals <74567285+oladaporoyals@users.noreply.github.com>
2023-08-06 00:04:57 +0100

    Create starwars-ilorin.py
    a basic python game probably what every one builds first

---
+++ starwars-ilorin.py
@@ -0,0 +1,29 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Star War Ipata")
+
+BG = pygame.transform.scale(pygame.image.load("stage.jpg"), (WIDTH, HEIGHT))
+
+def draw():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()         
