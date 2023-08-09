5fd9517e2222aed4b39c4143afb8d80bb0186fca
oladaporoyals <74567285+oladaporoyals@users.noreply.github.com>
2023-08-09 09:56:55 +0100

    Update text-to-speech.py
    the program works now and the text are now converted to speech, 
    saving the speech in .mp3 file is still under works

--- text-to-speech.py
+++ text-to-speech.py
@@ -2,15 +2,29 @@ import pyttsx3
 import PyPDF4
 
 # Replace the pdf file with the path to your own PDF file
-pdf_path = r'C:\Users\DAPO\Documents\python projects\file.pdf'  # Use raw string to avoid escape sequence issues
-pdf_reader = PyPDF2.PdfFileReader(open(pdf_path, 'rb'))
+pdf_path = r'C:\Users\DAPO\Documents\python projects\file.pdf' 
+
+# Use raw string to avoid escape sequence issues
+
+#initialize the PDF reader
+pdf_reader = PyPDF4.PdfFileReader(open(pdf_path, 'rb'))
+
+#initialize the text to speech engine
 reader = pyttsx3.init()
 
-for page in range(pdf_reader.numPages):
+#get the page numbers in the PDF
+num_pages = pdf_reader.numPages
+
+
+for page in range(num_pages):
+    #extract the texts from the pdf page
     text = pdf_reader.getPage(page).extractText()
     legible_text = text.strip().replace('\n', ' ')
     print(legible_text)
+    
+    # this part says the contents of the text
     reader.say(legible_text)
+    reader.runAndWait()
 
     # Save the text to an MP3 file
     output_file = f'page_{page+1}.mp3'
